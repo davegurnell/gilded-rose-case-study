d8b891e4d8769ffc488676d6713f4825946e2039
Dave Gurnell <dave@underscore.io>
2019-03-08 16:25:43 +0000

    Update GildedRoseTest.scala

--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,12 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+class GildedRoseTest  extends WordSpec with Matchers {
+  "the update method" should {
+    "foo" in {
+      var items = List(new Item("foo", 0, 0))
+      GildedRose.updateQuality(items)
+      app.items.head.name should equal ("fixme")
+    }
+  }
 }
